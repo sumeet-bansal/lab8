4b4f91d35199fda5cb48cb8feb6646c530312e43
Sumeet Bansal <sumeetbansal@gmail.com>
2018-02-28 17:47:15 -0800

    added RSVP form

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -31,5 +31,17 @@ function initGestures() {
 
 // init RSVP form submit listener
 function initRSVPForm() {
-  // add your code here
+	$('#rsvpForm').submit(function(e) {
+		e.preventDefault();
+		console.log("submitting form...");
+		var rsvpEmail = $('#rsvpEmail').val();
+		$.post('addRSVP', {
+			rsvpEmail: rsvpEmail
+		}, postCallback);
+	});
+
+	function postCallback(res) {
+		alert("RSVP form successfully submitted!");
+		$('#rsvpEmail').val('');
+	}
 }
--- routes/rsvp.js
+++ routes/rsvp.js
@@ -1,10 +1,14 @@
 var data = {
-  rsvp: ['ixd@ucsd.edu']
+	rsvp: ['ixd@ucsd.edu']
 };
 
-/*
- * GET tasks page.
- */
 exports.adminView = function(req, res){
-  res.render('rsvp', data);
+	res.render('rsvp', data);
 };
+
+exports.addRSVP = function(req, res) {
+	var rsvpEmail = req.body.rsvpEmail;
+	console.log(rsvpEmail);
+	data.rsvp.push(rsvpEmail);
+	res.send(rsvpEmail);
+}
