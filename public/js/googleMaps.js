f5256aafd9eda9003c328fc62bab3753b2c0223d
Sumeet Bansal <sumeetbansal@gmail.com>
2018-02-28 17:13:22 -0800

    added Google Maps view

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,3 +1,14 @@
 function initMap() {
-	// add your code here
+	var ucsd_ltlng = {lat:32.8789272, lng:-117.2370064};
+
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: ucsd_ltlng,
+		zoom: 15
+	});
+
+	var marker = new google.maps.Marker({
+		position: ucsd_ltlng,
+		map: map,
+		title: "UCSD"
+	});
 }
